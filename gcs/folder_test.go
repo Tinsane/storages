82c80265188987ce7a2c74ac19b8d79e5f538e92
akartasov <agneum@gmail.com>
2020-09-08 14:49:18 +0700

    Add tests

--- gcs/folder_test.go
+++ gcs/folder_test.go
@@ -1,8 +1,13 @@
 package gcs
 
 import (
-	"github.com/wal-g/storages/storage"
+	"errors"
 	"testing"
+	"time"
+
+	gcs "cloud.google.com/go/storage"
+
+	"github.com/wal-g/storages/storage"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -32,4 +37,59 @@ func TestGSExactFolder(t *testing.T) {
 	assert.NoError(t, err)
 
 	storage.RunFolderTest(storageFolder, t)
+}
+
+type fakeReader struct{}
+
+func (f fakeReader) Read(_ []byte) (int, error) {
+	return 0, errors.New("failed to fake read")
+}
+
+func TestUploadingRetries(t *testing.T) {
+	MaxRetries = 3
+	folder := Folder{
+		bucket:         &gcs.BucketHandle{},
+		path:           "path",
+		contextTimeout: 2,
+	}
+
+	err := folder.PutObject("name", fakeReader{})
+	assert.EqualError(t, err, "GCS error : Unable to copy to object: failed to fake read")
+}
+
+func TestJitterDelay(t *testing.T) {
+	baseDelay := time.Second
+	delay := getJitterDelay(baseDelay)
+
+	assert.GreaterOrEqual(t, int64(delay), int64(baseDelay))
+	assert.LessOrEqual(t, int64(delay), int64(2*baseDelay))
+}
+
+func TestMinDuration(t *testing.T) {
+	testCases := []struct {
+		duration1           time.Duration
+		duration2           time.Duration
+		expectedMinDuration time.Duration
+	}{
+		{
+			duration1:           time.Second,
+			duration2:           5 * time.Second,
+			expectedMinDuration: time.Second,
+		},
+		{
+			duration1:           3 * time.Second,
+			duration2:           2 * time.Second,
+			expectedMinDuration: 2 * time.Second,
+		},
+		{
+			duration1:           time.Second,
+			duration2:           time.Second,
+			expectedMinDuration: time.Second,
+		},
+	}
+
+	for _, tc := range testCases {
+		result := minDuration(tc.duration1, tc.duration2)
+		assert.Equal(t, tc.expectedMinDuration, result)
+	}
 }
