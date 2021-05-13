4337b0fe4a0f4a708896010048a448a9303f0af0
Roman Vasilyev <roman@lonje.com>
2021-05-13 14:39:00 -0700

    SSE Customer key

--- s3/folder.go
+++ s3/folder.go
@@ -1,15 +1,16 @@
 package s3
 
 import (
+	"io"
+	"strconv"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/pkg/errors"
 	"github.com/wal-g/storages/storage"
-	"io"
-	"strconv"
-	"strings"
 )
 
 const (
@@ -25,6 +26,7 @@ const (
 	SecretKeySetting         = "AWS_SECRET_KEY"
 	SessionTokenSetting      = "AWS_SESSION_TOKEN"
 	SseSetting               = "S3_SSE"
+	SseCSetting              = "S3_SSE_C"
 	SseKmsIdSetting          = "S3_SSE_KMS_ID"
 	StorageClassSetting      = "S3_STORAGE_CLASS"
 	UploadConcurrencySetting = "UPLOAD_CONCURRENCY"
